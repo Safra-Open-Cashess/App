7252eba190993c1a5fe35a2cc93191b3f4986b83
unknown <wilfaustino@gmail.com>
2022-04-03 16:41:42 -0300

    Layout frontend

---
+++ App - Frontend/js/07-redirecionamento.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    setTimeout(() => {
+        document.location.href = './08-banco-safra.html';
+    }, 1500);
+};
---
+++ App - Frontend/js/09-redirecionamento.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    setTimeout(() => {
+        document.location.href = './10-sucesso.html';
+    }, 2500);
+};
